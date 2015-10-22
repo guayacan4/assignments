989d60999e19a32f71505a9452dbdab1fd1b70d2
guayacan4 <hj.santos@me.com>
2015-10-21 20:57:17 -0400

    Added the chapter_assignment.rb file

--- chapter_assignment.rb
+++ chapter_assignment.rb
@@ -3,4 +3,4 @@ puts "Table of Contents".center (line_width)
 puts "=================================================="
 puts "Chapter 1: Get Started                      Page 1".ljust ( line_width )
 puts "Chapter 2: All about cats                   Page 3".ljust ( line_width )
-puts "Chapter 3: The parts of a bicycle           Page 9".ljust ( line_width )
+puts "Chapter 3: The parts of a bicycle           Page 4".ljust ( line_width )
