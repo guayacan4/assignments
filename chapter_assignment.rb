1fe308026724b7dff032dd264caa29633a3b133b
guayacan4 <hj.santos@me.com>
2015-10-21 22:06:31 -0400

    Change the complete program. Added variables and change the line_width

--- chapter_assignment.rb
+++ chapter_assignment.rb
@@ -1,6 +1,16 @@
-line_width = 50
+line_width = 45
 puts "Table of Contents".center (line_width)
-puts "=================================================="
-puts "Chapter 1: Get Started                      Page 1".ljust ( line_width )
-puts "Chapter 2: All about cats                   Page 3".ljust ( line_width )
-puts "Chapter 3: The parts of a bicycle           Page 4".ljust ( line_width )
+puts "==================================================="
+
+chapter1 = "Chapter 1: Get Started".ljust ( line_width )
+page1 = "Page 1".ljust ( line_width )
+
+chapter2 = "Chapter 2: All about cats".ljust ( line_width )
+page3 = "Page 3".ljust ( line_width )
+
+chapter3 = "Chapter 3: The parts of a bicycle".ljust ( line_width )
+page4 = "Page 4".ljust ( line_width )
+
+puts chapter1 + page1
+puts chapter2 + page3
+puts chapter3 + page4
