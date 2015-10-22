c2f97de39823b2ecb804f09d2881405896d46d11
guayacan4 <hj.santos@me.com>
2015-10-22 00:00:17 -0400

    Added variables for the lengths of the strings

--- chapter_assignment.rb
+++ chapter_assignment.rb
@@ -1,17 +1,19 @@
-line_width = 45
-line_width2 = 23
+line_width = 51
+
 puts "Table of Contents".center (line_width)
-puts "==================================================="
+puts "=" * line_width
 
-chapter1 = "Chapter 1: Get Started".ljust ( line_width )
-page1 = "Page 1".ljust ( line_width2 )
-
-chapter2 = "Chapter 2: All about cats".ljust ( line_width )
-page3 = "Page 3".ljust ( line_width )
-
-chapter3 = "Chapter 3: The parts of a bicycle".ljust ( line_width )
-page4 = "Page 4".ljust ( line_width )
-
+chapter_1_length = "Chapter 1: Get Started".length
+chapter1 = "Chapter 1: Get Started".ljust ( line_width - chapter_1_length )
+page1 = "Page 1".rjust ( chapter_1_length )
 puts chapter1 + page1
+
+chapter_2_length = "Chapter 2: All about cats".length
+chapter2 = "Chapter 2: All about cats".ljust ( line_width - chapter_2_length )
+page3 = "Page 3".rjust ( chapter_2_length )
 puts chapter2 + page3
+
+chapter_3_length = "Chapter 3: The parts of a bicycle".length
+chapter3 = "Chapter 3: The parts of a bicycle".ljust ( line_width - chapter_3_length )
+page4 = "Page 4".rjust ( line_width - chapter_3_length )
 puts chapter3 + page4
