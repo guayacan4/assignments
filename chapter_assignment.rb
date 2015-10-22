1e3a5e559b5e02236374121a496c7f13035dd758
guayacan4 <hj.santos@me.com>
2015-10-21 20:35:21 -0400

    Chapter assignment added

---
+++ chapter_assignment.rb
@@ -0,0 +1,6 @@
+line_width = 50
+puts "Table of Contents".center (line_width)
+puts "=================================================="
+puts "Chapter 1: Get Started                      Page 1".ljust ( line_width )
+puts "Chapter 2: All about cats                   Page 3".ljust ( line_width )
+puts "Chapter 3: The parts of a bicycle           Page 9".ljust ( line_width )
