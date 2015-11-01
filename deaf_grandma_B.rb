0c38d8b8696d9c5c02f25e2733dbbba20e46a113
guayacan4 <hj.santos@me.com>
2015-11-01 18:02:16 -0400

    Added deaf grandma II

---
+++ deaf_grandma_B.rb
@@ -0,0 +1,28 @@
+#Program to speak to deaf grandma B
+
+year = 1942
+print "You: "
+to_grandma = gets.chomp
+counter = 0
+
+while counter != 2
+  if to_grandma == to_grandma.downcase
+    puts "Grandma: WHAT!!! CAN'T HEAR YOU."
+    puts ""
+  elsif to_grandma == to_grandma.upcase && year >= 1912
+    puts "Grandma: NOT SINCE, " + year.to_s + "!"
+    puts ""
+    year -= 1
+  elsif to_grandma == to_grandma.upcase && year <= 1911
+    puts "Grandma: NOT SINCE, 1912!"
+    puts ""
+  end
+
+  if to_grandma == "GOODBYE"
+    counter += 1
+  end
+
+  print "You: "
+  to_grandma = gets.chomp
+
+  end
