bdf5f4a4285655d581a1a7bc8b5ac0dc1617875f
guayacan4 <hj.santos@me.com>
2015-11-01 12:06:54 -0400

    Change code to take inputs from the user

--- table_of_content_arrays.rb
+++ table_of_content_arrays.rb
@@ -1,37 +1,39 @@
 line_width = 51
 
-number_of_chapters = 3
+print "How many chapters are you going to add? "
+number_of_chapters = gets.chomp.to_i
 chapters = []
 pages = []
+x = 1 #this variable is defined to increment the chapter number in the next while loop
 
 while number_of_chapters > 0
-  print "What is the name of this chapter? "
+
+  print "What is the name of chapter #" + x.to_s + "? "
   chapter = gets.chomp
-  chapters.push << chapter
+  chapters << chapter
+  x += 1
 
   print "What page does this chapter starts? "
   page = gets.chomp
-  pages.push << page
+  pages << page
   puts ""
 
   number_of_chapters -= 1
-
 end
 
 puts "Table of Contents".center (line_width)
 puts "=" * line_width
 
-chapter_1_length = chapters[0].length
-chapter1 = chapters[0].ljust ( chapter_1_length )
-page1 = ("Page " + pages[0].to_s).rjust ( line_width - chapter_1_length )
-puts chapter1 + page1
+    i = 0 #this is a counter variable to take each element on the pages array
+    z = 1 #this variable is to increase by 1 the chapter number in the next block
 
-chapter_2_length = chapters[1].length
-chapter2 = chapters[1].ljust ( chapter_2_length )
-page2 = ("Page " + pages[1].to_s).rjust ( line_width - chapter_2_length )
-puts chapter2 + page2
+    chapters.each do |chapt|
+    chapter_name = "Chapter " + z.to_s + ": " + chapt
+    chapters_length = chapter_name.length
+    page_number = "Page " + pages[i]
+    pages_length = page_number.length
 
-chapter_3_length = chapters[2].length
-chapter3 = chapters[2].ljust ( chapter_3_length )
-page3 = ("Page " + pages[2].to_s).rjust ( line_width - chapter_3_length )
-puts chapter3 + page3
+    puts chapter_name + " "*(line_width - chapters_length - pages_length) + page_number
+    i += 1
+    z += 1
+      end
